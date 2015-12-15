ffe854fdf4b1a6f4a78fb651ca19dd5ecb735015
mafiapatch <g8071107@gmail.com>
2015-12-16 00:36:46 +0800

    select person

--- assets/script.js
+++ assets/script.js
@@ -3,4 +3,14 @@ $(document).ready(function() {
 		$('.person .gray_mask').transition('fade left');
 		window.location.href = 'bird.html';
 	});
+
+	$('#person_list .mafia').click(function() {
+		window.location.href = 'mafia.html';
+	});
+
+	$('#person_list .bird').click(function() {
+		window.location.href = 'bird.html';
+	});
+
+	$('.choose_person').popup();
 });
