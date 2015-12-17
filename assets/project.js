b7a5c5bb4e657763fd9786b8c6c7683883eee6ad
mafiapatch <g8071107@gmail.com>
2015-12-17 21:28:40 +0800

    test

--- assets/project.js
+++ assets/project.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-	
+
+	$('.back-btn').click(function() {
+		$('.project_main').load("http://unnamed.taipei/project_content.html");
+	});
+
 	$('.list-item').click(function() {
 		$('.list-item').transition('scale');
 		$('.project_main').load("http://unnamed.taipei/badboss.html");
