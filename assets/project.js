0481b3ca899b4efac1d25e464d856de7e9f59cdb
mafiapatch <g8071107@gmail.com>
2015-12-17 23:06:13 +0800

    smooth~

--- assets/project.js
+++ assets/project.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
 
+	$('.back-btn').click(function() {
+		$('.project-data').transition({
+		    animation : 'scale',
+		    onComplete  : function() {
+		      $('.project_main').transition('scale');
+		      $('.project_main').addClass("stackable");
+		    }
+		  });
+	});
+
 	$('.card.badboss').click(function() {
 		$('.project_main').transition({
 		    animation : 'scale',
-		    complete  : function() {
+		    onComplete  : function() {
+		      $('.project_main').removeClass("stackable");
 		      $('.badboss-info').transition('scale');
 		    }
 		  });
