8fc8951bb649333bd1ab085a65f2aa6a6ca11cb8
mafiapatch <g8071107@gmail.com>
2015-12-17 21:22:43 +0800

    test

--- assets/project.js
+++ assets/project.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-	$('.project_main').load("http://unnamed.taipei/project_content.html");
-
+	
 	$('.list-item').click(function() {
 		$('.list-item').transition('scale');
 		$('.project_main').load("http://unnamed.taipei/badboss.html");
