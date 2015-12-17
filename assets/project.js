0fc77b4f7186eb96064d49e6ab9b323bdff9920c
mafiapatch <g8071107@gmail.com>
2015-12-17 21:34:28 +0800

    test

--- assets/project.js
+++ assets/project.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
 
-	$('.back-btn').click(function() {
-		$('.project_main').load("http://unnamed.taipei/project_content.html");
-	});
-
 	$('.list-item').click(function() {
-		
+
 		$('.list-item').transition({
 			animation  : 'scale',
     		onComplete : function() {
