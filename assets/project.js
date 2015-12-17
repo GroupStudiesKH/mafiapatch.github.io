e1c1db945e324f0527a17b51af4b737d9e50b433
mafiapatch <g8071107@gmail.com>
2015-12-17 22:44:52 +0800

    test

--- assets/project.js
+++ assets/project.js
@@ -1,14 +1,5 @@
 $(window).load(function() {
 
-	$('.back-btn').click(function() {
-		$('.project-data').transition({
-		    animation : 'scale',
-		    onComplete  : function() {
-		      $(".project_main").load("http://unnamed.taipei/project_main.html");
-		  	}
-		});
-	});
-
 	$('.card.badboss').click(function() {
 		$('.list-item').transition({
 		    animation : 'scale',
