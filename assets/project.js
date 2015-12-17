ff3d7be4a62cfb6ec11e634322ced7e5593c952f
mafiapatch <g8071107@gmail.com>
2015-12-17 22:37:04 +0800

    test

--- assets/project.js
+++ assets/project.js
@@ -1,14 +1,5 @@
 $(document).ready(function() {
 
-	$('.back-btn').click(function() {
-		$('.project-data').transition({
-		    animation : 'scale',
-		    onComplete  : function() {
-		      $(".project_main").load("http://unnamed.taipei/project_main.html");
-		  	}
-		});
-	});
-
 	$('.card.badboss').click(function() {
 		$('.list-item').transition({
 		    animation : 'scale',
