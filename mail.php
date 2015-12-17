0b9ae1e2fb2fff9e445850d6d53bcba089b32af0
徐章嘉 <g8071107@gmail.com>
2015-12-17 11:23:27 +0800

    可以寄信了

---
+++ mail.php
@@ -0,0 +1,23 @@
+<?
+# Include the Autoloader (see "Libraries" for install instructions)
+require 'vendor/autoload.php';
+use Mailgun\Mailgun;
+
+$nickname=$_POST[nickname];
+$email=$_POST[email];
+$content=$_POST[content];
+
+# Instantiate the client.
+$mgClient = new Mailgun('key-6487c78c905dc041f7cb8e589d84de2d');
+$domain = "sandbox18191d042786489d8483f6dbbe361c3c.mailgun.org";
+
+# Make the call to the client.
+$result = $mgClient->sendMessage($domain, array(
+    'from'    => $email,
+    'to'      => 'cch@unnamed.taipei',
+    'subject' => $nickname.'寄來的問題',
+    'text'    => $content
+));
+
+echo "<script>window.history.back();</script>";
+?>
