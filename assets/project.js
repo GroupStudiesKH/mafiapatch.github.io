d0327538c8ade41cbdc245ec5bf7991fcd717710
mafiapatch <g8071107@gmail.com>
2015-12-17 21:32:42 +0800

    test

--- assets/project.js
+++ assets/project.js
@@ -5,8 +5,14 @@ $(document).ready(function() {
 	});
 
 	$('.list-item').click(function() {
-		$('.list-item').transition('scale');
-		$('.project_main').load("http://unnamed.taipei/badboss.html");
+		
+		$('.list-item').transition({
+			animation  : 'scale',
+    		onComplete : function() {
+		      $('.project_main').load("http://unnamed.taipei/badboss.html");
+		    }
+		});
+		
 	});
 
 });
