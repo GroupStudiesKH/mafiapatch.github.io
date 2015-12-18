b245f7b2ec5debc72112d1b3a9ffec0ddd1210a7
mafiapatch <g8071107@gmail.com>
2015-12-18 23:51:51 +0800

    maofa&takeit

---
+++ assets/maofa.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+	$('.maofa-info .back-btn').click(function() {
+		$('.maofa-info').transition({
+		    animation : 'scale',
+		    onComplete  : function() {
+		      $('.project_main').transition('scale');
+		      $('.project_main').addClass("stackable");
+		    }
+		  });
+	});
+
+	$('.card.maofa').click(function() {
+		$('.project_main').transition({
+		    animation : 'scale',
+		    onComplete  : function() {
+		      $('.project_main').removeClass("stackable");
+		      $('.maofa-info').transition('scale');
+		    }
+		  });
+	});
+
+	var $imgs = $('.maofa-info .card .image img'), current = 0;
+	    
+    var nextImage = function() {
+        if (current >= $imgs.length){current = 0;};
+        $imgs.eq(current++).fadeIn(function() {
+        $(this).delay(3000).fadeOut(nextImage);
+	    //console.log(current);
+	    })
+	};
+	nextImage();
+
+});
