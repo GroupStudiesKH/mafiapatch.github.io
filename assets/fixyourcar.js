fa84695b7b3e650e233890e2067e1e7e8d99c3c6
mafiapatch <g8071107@gmail.com>
2015-12-19 00:28:53 +0800

    fixyourcar

---
+++ assets/fixyourcar.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+	$('.fixyourcar-info .back-btn').click(function() {
+		$('.fixyourcar-info').transition({
+		    animation : 'scale',
+		    onComplete  : function() {
+		      $('.project_main').transition('scale');
+		      $('.project_main').addClass("stackable");
+		    }
+		  });
+	});
+
+	$('.card.fixyourcar').click(function() {
+		$('.project_main').transition({
+		    animation : 'scale',
+		    onComplete  : function() {
+		      $('.project_main').removeClass("stackable");
+		      $('.fixyourcar-info').transition('scale');
+		    }
+		  });
+	});
+
+	var $imgs = $('.fixyourcar-info .card .image img'), current = 0;
+	    
+    var nextImage = function() {
+        if (current >= $imgs.length){current = 0;};
+        $imgs.eq(current++).fadeIn(function() {
+        $(this).delay(3000).fadeOut(nextImage);
+	    //console.log(current);
+	    })
+	};
+	nextImage();
+
+});
